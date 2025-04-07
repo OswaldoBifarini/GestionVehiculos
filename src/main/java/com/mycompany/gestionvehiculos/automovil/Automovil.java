bd7586bba05f2f519232836a8b9a7f7905ecfff5
JIrleidySantos <jirleidysantosm@gmail.com>
2025-04-06 23:20:33 -0500

    Clase vehiculos y automovil

--- src/main/java/com/mycompany/gestionvehiculos/automovil/Automovil.java
+++ src/main/java/com/mycompany/gestionvehiculos/automovil/Automovil.java
@@ -4,10 +4,35 @@
  */
 package com.mycompany.gestionvehiculos.automovil;
 
+import com.mycompany.gestionvehiculos.vehiculo.Vehiculo;
+
 /**
  *
  * @author DELL
  */
-public class Automovil {
-    
+class Automovil extends Vehiculo {
+
+    private int numPuertas;
+
+    public Automovil(String marca, String modelo, int año, int numPuertas) {
+        super(marca, modelo, año);
+        this.numPuertas = numPuertas;
+    }
+
+    public Automovil() {
+    }
+
+    public int getNumPuertas() {
+        return this.numPuertas;
+    }
+
+    public void setNumPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.println("Numero Puertas: " + numPuertas);
+    }
 }
--- src/main/java/com/mycompany/gestionvehiculos/vehiculo/Vehiculo.java
+++ src/main/java/com/mycompany/gestionvehiculos/vehiculo/Vehiculo.java
@@ -9,5 +9,40 @@ package com.mycompany.gestionvehiculos.vehiculo;
  * @author DELL
  */
 public class Vehiculo {
-    
+    private String marca;
+     private String modelo;
+     private int año;
+ 
+     public Vehiculo(String marca, String modelo, int año) {
+         this.marca = marca;
+         this.modelo = modelo;
+         this.año = año;
+     }
+ 
+     public Vehiculo() {
+     }
+ 
+    public String getMarca() {
+        return this.marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public int getAño() {
+        return año;
+    }
+    public void setAño(int año) {
+         this.año = año;
+     }
+ 
+     public void mostrarInfo() {
+         System.out.println("Marca: " + marca + ", Modelo: " + modelo + ", Año: " + año);
+     }
+ 
 }
